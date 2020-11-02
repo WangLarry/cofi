32ac2cbec612453d3467046e69b47a2d6309bf60
Gal Havivi <gal.havivi@oath.com>
2020-11-02 09:28:08 +0200

    Updates

---
+++ react-form/precache-manifest.c614827a496aa918754ffaa7c5296d1d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d905c9326b6f7f6d575c30c6c4356825",
+    "url": "https://galhavivi.github.io/cofi/react-form/index.html"
+  },
+  {
+    "revision": "a96cb862cf48d9739298",
+    "url": "https://galhavivi.github.io/cofi/react-form/static/css/main.99903ae4.css"
+  },
+  {
+    "revision": "a96cb862cf48d9739298",
+    "url": "https://galhavivi.github.io/cofi/react-form/static/js/main.b08f3df0.js"
+  },
+  {
+    "revision": "07ab01bb7bfde7c15bdfda6cac90fd10",
+    "url": "https://galhavivi.github.io/cofi/react-form/static/js/main.b08f3df0.js.LICENSE.txt"
+  },
+  {
+    "revision": "8387712a669dfe22884f656d81857331",
+    "url": "https://galhavivi.github.io/cofi/react-form/static/media/logo.8387712a.svg"
+  }
+]);
