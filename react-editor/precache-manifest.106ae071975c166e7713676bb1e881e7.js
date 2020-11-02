026b27da0b6677cff3fc93ee4bad32a0df0d3d55
Gal Havivi <gal.havivi@oath.com>
2020-11-02 09:35:38 +0200

    Updates

---
+++ react-editor/precache-manifest.106ae071975c166e7713676bb1e881e7.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc5c6aaf7c7d06da9df58f9ec70078aa",
+    "url": "https://galhavivi.github.io/cofi/react-editor/index.html"
+  },
+  {
+    "revision": "4322da0abf036b455deb",
+    "url": "https://galhavivi.github.io/cofi/react-editor/static/js/main.dc7263a6.js"
+  },
+  {
+    "revision": "07ab01bb7bfde7c15bdfda6cac90fd10",
+    "url": "https://galhavivi.github.io/cofi/react-editor/static/js/main.dc7263a6.js.LICENSE.txt"
+  },
+  {
+    "revision": "8387712a669dfe22884f656d81857331",
+    "url": "https://galhavivi.github.io/cofi/react-editor/static/media/logo.8387712a.svg"
+  }
+]);
